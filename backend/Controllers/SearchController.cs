dca0cd8e37c7d18d5e0a39975397b03d84df50e5
Mark3vich <mango.2004@yandex.ru>
2023-09-28 18:16:28 +0300

    search

---
+++ backend/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public SearchController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> searchProduct(string searchString)
+        {
+            searchString = searchString.Trim();
+            var products = _context.Product.Where(product => product.ProductName == searchString);
+            return Ok(products);
+        }
+    }
+}
